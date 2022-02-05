6b4f708d2c05ece938db787645de1062a2d77a07
Eugene Roy <box.rz.box@gmail.com>
2022-02-05 13:34:12 +0200

    link between pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import Link from "next/link";
+
 const HomePage = () => {
-  return <h1>Home page</h1>;
+  return (
+    <>
+      <h1>Home page</h1>
+      <ul>
+        <li>
+          <Link href="/news/react_course">New React Course is HERE!</Link>
+        </li>
+        <li>
+          <Link href="/news/nodeUpdate">Look at new Node version.</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default HomePage;
