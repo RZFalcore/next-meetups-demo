0f5a09129481e929f6cb362af587d26754fb3037
Eugene Roy <box.rz.box@gmail.com>
2022-02-05 20:56:52 +0200

    start filling homepage, added dummy meetups

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "1",
+    title: "Team place",
+    image:
+      "http://cdn.cnn.com/cnnnext/dam/assets/170123102557-beautiful-india-valley-of-flowers-flickr-alosh-bennett-4951216953.jpg",
+    address: "India, Some mountains :)",
+    description: "Our new place!",
+  },
+  {
+    id: "2",
+    title: "New open space",
+    image: "https://media.timeout.com/images/105184577/750/562/image.jpg",
+    address: "Some city, top flour :)",
+    description: "Our new place!",
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
+
+export default HomePage;
