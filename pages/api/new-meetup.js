c58727a0b462dc6baf3eafbfb00489c71732a696
Eugene Roy <box.rz.box@gmail.com>
2022-02-08 14:07:09 +0200

    added  mongodb connection

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,37 @@
+import { MongoClient } from "mongodb";
+
 const handler = (req, res) => {
   if (req.method !== "POST") return;
 
-  const { title, image, address, description } = req.body.data;
+  const data = req.body;
   console.log("Data: ", title, image, address, description);
+
+  const uri =
+    "mongodb+srv://rzfalcore:a7hA4GtutCPtmhR@cluster0.tqc39.mongodb.net/meetups?retryWrites=true&w=majority";
+
+  try {
+    const client = await MongoClient.connect(uri);
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+    res.status(201).json({ message: "Meetup added!" });
+  } catch (error) {
+    console.log(error.message);
+    console.log(error);
+  }
 };
 
 export default handler;
+
+// const { MongoClient } = require('mongodb');
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
